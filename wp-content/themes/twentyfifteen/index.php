6fa0d602529277a72c18f0723709ebae6bb1e3b4
Garrett Estrin <garrett.estrin@gmail.com>
2018-03-24 13:29:13 -0700

    added logo to landing page

--- wp-content/themes/twentyfifteen/index.php
+++ wp-content/themes/twentyfifteen/index.php
@@ -1,11 +1,19 @@
 <?php
 ?>
 
+<!DOCTYPE html>
 <html>
-	<head>
-
-	</head>
-	<body>
-		ErikaVanGogh.com
-	</body>
+  <head>
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>ErkaVanGogh.com</title>
+  <meta name="description" content="description here">
+  <meta name="keywords" content="keywords,here">
+  <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+  <link rel="stylesheet" href="stylesheet.css" type="text/css">
+  </head>
+  <body style="text-align: center;display: flex; align-items=center;">
+	<img src="<?php bloginfo('stylesheet_directory')?>/assets/images/logo.png" alt="" style="    display: block;
+    margin: 0 auto;">
+  </body>
 </html>
