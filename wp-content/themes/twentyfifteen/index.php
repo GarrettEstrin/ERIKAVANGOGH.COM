09a4e992193c9e284696f60bc1da911eca026773
Garrett Estrin <garrett.estrin@gmail.com>
2018-03-24 22:04:27 -0700

    updated landing page

--- wp-content/themes/twentyfifteen/index.php
+++ wp-content/themes/twentyfifteen/index.php
@@ -5,15 +5,31 @@
 <html>
   <head>
     <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=600">
     <title>ErkaVanGogh.com</title>
-  <meta name="description" content="description here">
-  <meta name="keywords" content="keywords,here">
-  <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
-  <link rel="stylesheet" href="stylesheet.css" type="text/css">
+    <meta name="description" content="description here">
+    <meta name="keywords" content="keywords,here">
+    <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+    <link rel="stylesheet" href="stylesheet.css" type="text/css">
+    <style>
+    html, body {
+      height: 100%;
+      width: 100%;
+      box-sizing: border-box;
+      overflow: hidden;
+    }
+    body {
+      text-align: center;
+      display: flex; 
+      align-items:center;
+    }
+    .logo {
+      display: block;
+      margin: 0 auto;
+    }
+    </style>
   </head>
-  <body style="text-align: center;display: flex; align-items=center;">
-	<img src="<?php bloginfo('stylesheet_directory')?>/assets/images/logo.png" alt="" style="    display: block;
-    margin: 0 auto;">
+  <body>
+	<img src="<?php bloginfo('stylesheet_directory')?>/assets/images/logo.png" alt="" class="logo">
   </body>
 </html>
